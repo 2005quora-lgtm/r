ec9803ef41e6e9d6a7a929cb74007a9ae967086f
sun-chingwoo <sunchingwoo@gmail.com>
2025-12-14 14:28:57 +0530

    styled

--- frontend/src/components/Createcard.jsx
+++ frontend/src/components/Createcard.jsx
@@ -1,13 +1,18 @@
 const Createcard = ({ card }) => {
   return (
-      <div className="bg-gray-500 m-5 rounded-md border border-white-500 overflow-hidden">
+      <div className={`${card.booked? "bg-gray-800 text-white m-5 rounded-md border border-yellow-500 overflow-hidden text-center transition-all duration-300 transform hover:scale-105 hover:bg-red-600":"bg-gray-800 text-white m-5 rounded-md border border-yellow-500 overflow-hidden text-center transition-all duration-300 transform hover:scale-105 hover:bg-green-600"}`}>
         <figure>
           <img src={card.image} alt={card.name} className="w-full h-48 object-cover" />
         </figure>
         <h2 className="text-xl font-bold mt-2">{card.name}</h2>
-        <div>{card.location}</div>
-        <b>{card.price}</b>
-        <p>{card.booked ? "Booked" : "Available"}</p>
+        <div className="text-gray-300">{card.location}</div>
+        <b className="text-gray-400">{card.price}</b>
+        <p
+  className={`p-2 border rounded-sm ${card.booked ? "bg-red-600" : "bg-green-600"} text-white`}
+>
+  {card.booked ? "Booked" : "Available"}
+</p>
+
       </div>
   )
 }
